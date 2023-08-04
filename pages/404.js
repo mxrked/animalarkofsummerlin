562d9363ed07199463eea51d1fcd0a1b74776789
Parker <pp101socials@gmail.com>
2023-08-04 14:59:08 -0400

    Created file

---
+++ pages/404.js
@@ -0,0 +1,18 @@
+// React/Next Imports
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+// Library Imports
+
+// Data/Functions/Images Imports
+
+// Component Imports
+
+// Style Imports
+// import "../assets/styles/modules/404/404.module.css";
+
+export default function NotFound() {
+  const router = useRouter();
+
+  return "404 Page";
+}
