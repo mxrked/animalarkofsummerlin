598ae88e49c0b51c9fb71fcfaa02566e82dce1ed
Parker <pp101socials@gmail.com>
2023-08-05 13:40:35 -0400

    Added cdns

--- assets/cdns/CDNBgs.js
+++ assets/cdns/CDNBgs.js
@@ -5,3 +5,7 @@
  */
 
 import { CDNBGReturn } from "./CDNReturns";
+
+const INDEX_TOP_BG = CDNBGReturn("index", "dog-outside", "webp");
+const INDEX_ABOUT_BG = CDNBGReturn("index", "group-of-dogs", "webp");
+const INDEX_CONTACT_BG = CDNBGReturn("index", "man-comforting-dog", "webp");
--- assets/cdns/CDNImgs.js
+++ assets/cdns/CDNImgs.js
@@ -5,3 +5,19 @@
  */
 
 import { CDNImgReturn } from "./CDNReturns";
+
+const PLACEHOLDER = CDNImgReturn("dogs", "placeholder", "png");
+
+const MERLIN_1 = CDNImgReturn("dogs", "merlin-1", "webp");
+const PHOENIX_1 = CDNImgReturn("dogs", "phoenix-1", "webp");
+const PHOENIX_2 = CDNImgReturn("dogs", "phoenix-2", "webp");
+const SOPHIE_1 = CDNImgReturn("dogs", "sophie-1", "webp");
+const SOPHIE_2 = CDNImgReturn("dogs", "sophie-2", "webp");
+const SOPHIE_3 = CDNImgReturn("dogs", "sophie-3", "webp");
+
+const DOG_ART_1 = CDNImgReturn("dog-art", "dog-art-1", "webp");
+const DOG_ART_2 = CDNImgReturn("dog-art", "dog-art-2", "webp");
+
+export { PLACEHOLDER };
+export { MERLIN_1, PHOENIX_1, PHOENIX_2, SOPHIE_1, SOPHIE_2, SOPHIE_3 };
+export { DOG_ART_1, DOG_ART_2 };
