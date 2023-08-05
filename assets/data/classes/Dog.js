96927711f4956b11539affbc81b770071968e36f
Parker <pp101socials@gmail.com>
2023-08-05 13:40:49 -0400

    Created file

--- assets/data/classes/Dog.js
+++ assets/data/classes/Dog.js
@@ -4,4 +4,44 @@
  *
  */
 
-export default class Dog {}
+export default class Dog {
+  constructor(
+    dogID,
+    dogName,
+    dogBreed,
+    dogWeight,
+    dogDesc,
+    dogImg,
+    dogAdoptStatus
+  ) {
+    this._dogID = dogID;
+    this._dogName = dogName;
+    this._dogBreed = dogBreed;
+    this._dogWeight = dogWeight;
+    this._dogDesc = dogDesc;
+    this._dogImg = dogImg;
+    this._dogAdoptStatus = dogAdoptStatus;
+  }
+
+  get getDogID() {
+    return this._dogID;
+  }
+  get getDogName() {
+    return this._dogName;
+  }
+  get getDogBreed() {
+    return this._dogBreed;
+  }
+  get getDogWeight() {
+    return this._dogWeight;
+  }
+  get getDogDesc() {
+    return this._dogDesc;
+  }
+  get getDogImg() {
+    return this._dogImg;
+  }
+  get getDogAdoptStatus() {
+    return this._dogAdoptStatus;
+  }
+}
