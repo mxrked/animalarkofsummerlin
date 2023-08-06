53f44e333b7d377c0927c1e9fee576490150a2a6
Parker <pp101socials@gmail.com>
2023-08-06 12:54:30 -0400

    Added cdns

--- assets/cdns/CDNIcons.js
+++ assets/cdns/CDNIcons.js
@@ -12,6 +12,7 @@ const FAVICON_16 = CDNIconReturn("tab-icons", "favicon-16x16", "png");
 const ATI = CDNIconReturn("tab-icons", "apple-touch-icon", "png");
 
 const LOGO = CDNIconReturn("logo", "logo", "webp");
+const LOGO_WITHOUT_TEXT = CDNIconReturn("logo", "logo-without-text", "webp");
 
 export { FAVICON, FAVICON_16, FAVICON_32, ATI };
-export { LOGO };
+export { LOGO, LOGO_WITHOUT_TEXT };
