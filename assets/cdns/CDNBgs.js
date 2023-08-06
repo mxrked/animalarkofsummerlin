c8c16644f5a21fba7da55f7efc3a568a4197a7e9
Parker <pp101socials@gmail.com>
2023-08-06 10:32:36 -0400

    Added CDNs

--- assets/cdns/CDNBgs.js
+++ assets/cdns/CDNBgs.js
@@ -6,6 +6,11 @@
 
 import { CDNBGReturn } from "./CDNReturns";
 
+const NAV_TOP_GRADIENT_BG = CDNBGReturn("gradients", "yellow-gradient", "webp");
+
 const INDEX_TOP_BG = CDNBGReturn("index", "dog-outside", "webp");
 const INDEX_ABOUT_BG = CDNBGReturn("index", "group-of-dogs", "webp");
 const INDEX_CONTACT_BG = CDNBGReturn("index", "man-comforting-dog", "webp");
+
+export { NAV_TOP_GRADIENT_BG };
+export { INDEX_ABOUT_BG, INDEX_CONTACT_BG, INDEX_TOP_BG };
