edb0a106d1a007a0803011270c2e4f315d5226a5
Parker <pp101socials@gmail.com>
2023-08-09 12:19:32 -0400

    Added components

--- pages/index.js
+++ pages/index.js
@@ -16,10 +16,26 @@ import { MobileNav } from "@/assets/components/global/Nav/Mobile/MobileNav";
 import { MobileNavMenu } from "@/assets/components/global/Nav/Mobile/MobileNavMenu";
 import { IndexTop } from "@/assets/components/pages/Index/IndexTop";
 import { IndexAbout } from "@/assets/components/pages/Index/IndexAbout";
+import { IndexAdopt } from "@/assets/components/pages/Index/IndexAdopt";
 
 // Style Imports
 import "../assets/styles/modules/Index/Index.module.css";
-export default function Home() {
+
+export const getStaticProps = async () => {
+  const INDEX_DOGS_RES = await fetch(
+    "https://raw.githubusercontent.com/mxrked/freelance_projects_CDN/main/animalarkofsummerlin/json/dogs/IndexDogs.json"
+  );
+
+  const INDEX_DOGS_RES_DATA = await INDEX_DOGS_RES.json();
+
+  return {
+    props: {
+      index_dogs_data: INDEX_DOGS_RES_DATA,
+    },
+  };
+};
+
+export default function Home({ index_dogs_data }) {
   const router = useRouter();
 
   // Triggering exit animations
@@ -39,6 +55,7 @@ export default function Home() {
       <main id="PAGE_CNT">
         <IndexTop />
         <IndexAbout />
+        <IndexAdopt indexDogs={index_dogs_data} />
       </main>
     </div>
   );
