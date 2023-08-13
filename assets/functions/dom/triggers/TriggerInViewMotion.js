e217e72bb77ee057cdcc37ae2c0ba84aa398c187
Parker <pp101socials@gmail.com>
2023-08-13 12:10:24 -0400

    Added param comments

--- assets/functions/dom/triggers/TriggerInViewMotion.js
+++ assets/functions/dom/triggers/TriggerInViewMotion.js
@@ -1,6 +1,8 @@
 /**
  *
  *  This is used to trigger a inView framer-motion motion
+ *  @param {Object} controls - The controls variable(object) for the parent motion.div
+ *  @param {Object} inview - The inview variable(object) for the parent motion.div
  *
  */
 
