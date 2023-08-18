b29e81b6656cd566fd27add220a276a952b8de03
Parker <pp101socials@gmail.com>
2023-08-18 12:19:41 -0400

    Created file

--- assets/functions/data/email/CheckValidZip.js
+++ assets/functions/data/email/CheckValidZip.js
@@ -12,13 +12,16 @@ export default function CheckValidZip(zip) {
 
   // Checking if the value is exactly 5 digits
   if (ZIP_CODE.length !== 5) {
+    console.log("Invalid Zip Code.");
     return false;
   }
 
   // Checking if value is only numbers
   if (!/^\d+$/.test(ZIP_CODE)) {
+    console.log("Invalid Zip Code.");
     return false;
   }
 
+  console.log("Valid Zip Code!!!");
   return true;
 }
