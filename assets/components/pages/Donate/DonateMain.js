beff7fdd8e09fe16a7c3ffa0a238499c25e08a91
Parker <pp101socials@gmail.com>
2023-08-26 13:07:39 -0400

    Created file

--- assets/components/pages/Donate/DonateMain.js
+++ assets/components/pages/Donate/DonateMain.js
@@ -23,10 +23,42 @@ export const DonateMain = () => {
     TriggerInViewMotion(CONTROLS, INVIEW);
   }, [CONTROLS, INVIEW]);
 
+  // $5
+  // $15
+  // $30
+
   return (
     <section
       id="donateMain"
       className={`${styles.donate_main} overrides_DonateMain`}
-    ></section>
+    >
+      {" "}
+      <motion.div
+        ref={REF}
+        initial="hidden"
+        animate={CONTROLS}
+        variants={FADE_DOWN}
+        className={`${styles.donate_main_inner} fm-motion fade-down-fix fade-down`}
+      >
+        <h1 className="orientation-change-element half-second">
+          Type of Donations.
+        </h1>
+
+        <p className="orientation-change-element half-second">
+          Below is the different amounts you can donate to help our goal in
+          helping and rescuing dogs.
+        </p>
+
+        <ul className="orientation-change-element half-second">
+          <li>$5.00</li>
+          <li>$15.00</li>
+          <li>$30.00</li>
+        </ul>
+
+        <button className="orientation-change-element half-second">
+          <span>Donate Via PayPal</span>
+        </button>
+      </motion.div>
+    </section>
   );
 };
